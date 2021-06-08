950e90781f5b207f0b9e958fbe3a1e1336d75ff7
Dylan Nguyen <dynguye@gmail.com>
2021-06-07 21:34:08 -0400

    updates

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React from "react";
 // import { Link } from 'react-router-dom';
-import './main.css'
+import "./main.css";
 
-export default function Footer(){
-    return (
-        <footer>
-    
-        <div id="social_media">
-          <a href="https://github.com/dynguy" target="_blank" rel="noreferrer" class="btn contact-details"><i class="fab fa-github"></i></a>
-          <a href="https://www.linkedin.com/in/dynguy/" target="_blank" rel="noreferrer" class="btn contact-details"><i class="fab fa-linkedin"></i></a>
-        </div>
+export default function Footer() {
+  return (
+    <footer>
+      <div id="social_media">
+        <a
+          href="https://github.com/dynguy"
+          target="_blank"
+          rel="noreferrer"
+          class="btn contact-details"
+        >
+          <i class="fab fa-github"></i>{"  "}GitHub
+        </a>
         
-    
-      </footer>
-    )
+        <a
+          href="https://www.linkedin.com/in/dynguy/"
+          target="_blank"
+          rel="noreferrer"
+          class="btn contact-details"
+        >
+          <i class="fab fa-linkedin"></i>{"  "} Linkedin
+        </a>
+       
+        <a
+          href="assets/Resume2021.pdf"
+          target="_blank"
+          rel="noreferrer"
+          class="btn contact-details"
+        >
+          <i class="far fa-copy"></i>{" "}Resume
+        </a>
+        
+        <a
+          href="https://github.com/dynguy?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+          class="btn contact-details"
+        >
+          <i class="far fa-folder"></i>{" "}Projects
+        </a>
+        
+      </div>
+    </footer>
+  );
 }
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React from "react";
 // import { Link } from 'react-router-dom';
-import './main.css'
+import "./main.css";
 
-export default function Main(){
-    return (
-      <section id="box">
+export default function Main() {
+  return (
+    <section id="box">
       <div class="top-info-box">
         About Me
         <div class="intro">
-          <p> Hello! My name is Dylan Nguyen and I am a computer science student at the University of
-            Maryland, College Park.</p>
-          <p> I graduated from Montgomery College in 2020 with an AA in Computer Science.</p>
-          <p> Previously, I was a computer forensics research intern under NIST's <a href="https://www.nist.gov/itl/ssd/software-quality-group/computer-forensics-tool-testing-program-cftt" target="_blank" rel="noreferrer">CFTT</a> team. Here, I was able to dive
-            into the world of mobile device forensics and research using various data extraction digital tools (XRY Kiosk, Autopsy).</p>
-          <p> You can check out my <a href="assets/Resume2021.pdf" target="_blank">resume</a> and email me at
-            dynguye@umd.edu. </p>
+          <p>
+            {" "}
+            Hello! My name is Dylan Nguyen and I am a computer science student
+            at the University of Maryland, College Park.
+          </p>
+          <p>
+            {" "}
+            I graduated from Montgomery College in 2020 with an AA in Computer
+            Science.
+          </p>
+          <p>
+            {" "}
+            Previously, I was a computer forensics research intern under NIST's{" "}
+            <a
+              href="https://www.nist.gov/itl/ssd/software-quality-group/computer-forensics-tool-testing-program-cftt"
+              target="_blank"
+              rel="noreferrer"
+            >
+              CFTT
+            </a>{" "}
+            team. Here, I was able to dive into the world of mobile device
+            forensics and research using various data extraction digital tools
+            (XRY Kiosk, Autopsy).
+          </p>
+          <p>
+            {" "}
+            You can check out my resume and projects below! You can also email
+            me at dynguye@umd.edu.{" "}
+          </p>
         </div>
       </div>
     </section>
-    )
+  );
 }
