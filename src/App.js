e109eb57252b1c5f07feb987dba33ed9dd68d6cf
Dylan Nguyen <dynguye@gmail.com>
2021-06-13 23:37:26 -0400

    a lot of updates

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import Main from "./components/main/index.js";
 import Footer from "./components/footer/index.js";
-
-// import React, { Component } from "react";
-// import { HashRouter, Route } from "react-router-dom";
-
-
-import './App.css';
-
-
+import Projects from "./components/projects/index.js";
+import { HashRouter, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <center><Main /></center>
-      <center><Footer /></center>
+      {/* <center><Main /></center> */}
+
+      <HashRouter basename={process.env.PUBLIC_URL}>
+        <>
+          <center>
+            <Route path={"/"} component={Main} exact />
+          </center>
+          
+          <Route path={"/projects"} component={Projects} />
+        </>
+      </HashRouter>
+        <center>
+            <Footer />
+        </center>
     </div>
   );
 }
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,47 +1,99 @@
 import React from "react";
 // import { Link } from 'react-router-dom';
 import "./main.css";
+// import { NavLink } from "react-router-dom";
+import { Nav, NavItem, NavLink } from "reactstrap";
 
 export default function Footer() {
   return (
     <footer>
       <div id="social_media">
-        <a
+        {/* <a
           href="https://github.com/dynguy"
           target="_blank"
           rel="noreferrer"
           class="btn contact-details"
         >
-          <i class="fab fa-github"></i>{"  "}GitHub
+          <i class="fab fa-github"></i>
+          {"  "}GitHub
         </a>
-        
+
         <a
           href="https://www.linkedin.com/in/dynguy/"
           target="_blank"
           rel="noreferrer"
           class="btn contact-details"
         >
-          <i class="fab fa-linkedin"></i>{"  "} Linkedin
+          <i class="fab fa-linkedin"></i>
+          {"  "} Linkedin
         </a>
-       
+
         <a
           href="assets/Resume2021.pdf"
           target="_blank"
           rel="noreferrer"
           class="btn contact-details"
         >
-          <i class="far fa-copy"></i>{" "}Resume
+          <i class="far fa-address-card"></i> Resume
         </a>
-        
         <a
-          href="https://github.com/dynguy?tab=repositories"
+          href="projects/index.js"
           target="_blank"
           rel="noreferrer"
           class="btn contact-details"
         >
-          <i class="far fa-folder"></i>{" "}Projects
-        </a>
-        
+          <i class="far fa-folder-open"></i> Projects
+        </a> */}
+        <Nav navbar>
+          <NavItem>
+            <NavLink href="#/">
+              <a href="#/" rel="noreferrer" class="btn contact-details">
+                <i class="fas fa-home"></i>
+                {"  "}Home
+              </a>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <a
+              href="https://github.com/dynguy"
+              target="_blank"
+              rel="noreferrer"
+              class="btn contact-details"
+            >
+              <i class="fab fa-github"></i>
+              {"  "}GitHub
+            </a>
+          </NavItem>
+          <NavItem>
+            <a
+              href="https://www.linkedin.com/in/dynguy/"
+              target="_blank"
+              rel="noreferrer"
+              class="btn contact-details"
+            >
+              <i class="fab fa-linkedin"></i>
+              {"  "} Linkedin
+            </a>
+          </NavItem>
+          <NavItem>
+            <a
+              href="assets/Resume2021.pdf"
+              target="_blank"
+              rel="noreferrer"
+              class="btn contact-details"
+            >
+              <i class="far fa-address-card"></i> Resume
+            </a>
+          </NavItem>
+
+          <NavItem>
+            <NavLink href="#/projects/">
+              <a href="#/projects/" rel="noreferrer" class="btn contact-details">
+                <i class="far fa-folder-open"></i> Projects
+              </a>
+            </NavLink>
+          </NavItem>
+        </Nav>
       </div>
     </footer>
   );
