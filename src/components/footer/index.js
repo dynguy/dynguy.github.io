017b672314ad45bf6b78adb3a232bfa3a8655e22
Dylan Nguyen <dynguye@gmail.com>
2021-06-19 19:17:51 -0400

    updates

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Link } from 'react-router-dom';
-import "./main.css";
+import "./footer.css";
 // import { NavLink } from "react-router-dom";
 import { Nav, NavItem, NavLink } from "reactstrap";
 
@@ -8,42 +8,6 @@ export default function Footer() {
   return (
     <footer>
       <div id="social_media">
-        {/* <a
-          href="https://github.com/dynguy"
-          target="_blank"
-          rel="noreferrer"
-          class="btn contact-details"
-        >
-          <i class="fab fa-github"></i>
-          {"  "}GitHub
-        </a>
-
-        <a
-          href="https://www.linkedin.com/in/dynguy/"
-          target="_blank"
-          rel="noreferrer"
-          class="btn contact-details"
-        >
-          <i class="fab fa-linkedin"></i>
-          {"  "} Linkedin
-        </a>
-
-        <a
-          href="assets/Resume2021.pdf"
-          target="_blank"
-          rel="noreferrer"
-          class="btn contact-details"
-        >
-          <i class="far fa-address-card"></i> Resume
-        </a>
-        <a
-          href="projects/index.js"
-          target="_blank"
-          rel="noreferrer"
-          class="btn contact-details"
-        >
-          <i class="far fa-folder-open"></i> Projects
-        </a> */}
         <Nav navbar>
           <NavItem>
             <NavLink href="#/">
--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,10 +1,35 @@
 import React from "react";
 import "./projects.css";
 
-export default function Projects(){
-    return (
-        <div>
-            <p>Work in Progress!</p>
+export default function Projects() {
+  return (
+    <div class="projects">
+      <section class="project-box">
+      <style>{'body { background-color: red; }'}<div>Hello</div></style>
+        <div class="project-title-area">
+          
+        Predicting the Valence of Pop Songs through Spotify's Audio Features
         </div>
-    )
+        <div class="project-info">My final project for my data science class.</div>
+      </section>
+      <section class="project-box">
+        <div class="project-title-area">
+          Coronavirus Web-Scrapping Discord Bot
+        </div>
+        <div class="project-info">This is project 1 testing</div>
+      </section>
+      {/* <section class="project-box">
+        <div class="project-title-area">Alien Influx</div>
+        <div class="project-info">This is project 3 testing</div>
+      </section>
+      <section class="project-box">
+        <div class="project-title-area">CPS Test</div>
+        <div class="project-info">Clicks Per Second Test</div>
+      </section>
+      <section class="project-box">
+        <div class="project-title-area">CPS Test</div>
+        <div class="project-info">Clicks Per Second Test</div>
+      </section> */}
+    </div>
+  );
 }
